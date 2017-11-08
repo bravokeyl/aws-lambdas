d74449795d208ad7ab7132ed562b82a56b7dd841
bravokeyl <bravokeyl@gmail.com>
2017-11-08 18:47:16 +0530

    hour energy update

--- kmm-hour-energy/hr.sh
+++ kmm-hour-energy/hr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # https://pyz1xbouqb.execute-api.us-east-1.amazonaws.com/l/h?dhr=
-for number in 08
+for number in {00..23}
 do
-  dat="2017/10/29/"$number
+  dat="2017/11/01/"$number
   echo $dat
   pay='{"params":{"querystring":{"dhr":"';
   a='"}}}';
@@ -10,7 +10,7 @@ do
   echo $b
   aws lambda invoke --function-name kmm-hour-energy --payload $b "a.json"
   echo " "
-  read -p "Continuing in 2 Seconds...." -t 2
+  read -p "Continuing in 3 Seconds...." -t 3
   echo "Continuing ...."
 done
 exit 0
